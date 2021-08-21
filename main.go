08a13441529530be1bd4cb466fc6232af0875fd6
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-21 04:13:50 +0100

    Main Function

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import "github.com/Ad3bay0c/week4Curriculum/cli"
+
+// "fmt"
+
+// "github.com/Ad3bay0c/week4Curriculum/io_package"
+// "github.com/Ad3bay0c/week4Curriculum/json"
+
+func main() {
+	// Json
+	// json.Encrypt()
+
+	// json.Encode2()
+
+	// json.EncodeUsingMap()
+
+	// json.Decode()
+	// IO
+	// io_package.IoTesting()
+	// io_package.IoPipeTesting()
+	// io_package.InputReader()
+
+	// cli.Lst.All()
+	// // lst := cli.List{Content: "Read my curriculum"}
+
+	// cli.Lst.AddContent("Read Working with Json")
+	// cli.Lst.AddContent("Read my Curriculum")
+	// cli.Lst.AddContent("Solve little Algorithm")
+	// cli.Lst.AddContent("Review my Weekly Task")
+
+	// cli.Lst.All()
+	// fmt.Println("............Adding Content......")
+
+	// time.Sleep(time.Second * 2)
+	// cli.Lst.AddContent("Continue with the curriculum")
+	// cli.Lst.AddContent("Write a sample Task")
+	// cli.Lst.All()
+
+	// fmt.Println("............Testing Done......")
+
+	// time.Sleep(time.Second * 2)
+	// cli.Lst.Done(1)
+	// cli.Lst.Done(6)
+	// cli.Lst.All()
+
+	// fmt.Println("............Testing Undone........")
+	// cli.Lst.Undone(1)
+
+	cli.Lst.All()
+
+	// time.Sleep(time.Second * 2)
+
+	// fmt.Println("............Testing Clean Up........")
+	// cli.Lst.Done(2)
+	// time.Sleep(time.Second * 1)
+	// cli.Lst.Cleanup()
+
+	// cli.Lst.All()
+
+	// cli.CliTest()
+
+	// files.File()
+}
