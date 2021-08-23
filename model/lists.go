5ff8e38f5baaf7d61509a22499030d7f3a68a573
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-23 16:14:01 +0100

    Todo Lists Model Added

--- model/lists.go
+++ model/lists.go
@@ -1,1 +1,21 @@
 package model
+
+import (
+	"encoding/json"
+	"github.com/Ad3bay0c/week4Curriculum/cli"
+	"io/ioutil"
+)
+
+var Lst = cli.Lists{}
+
+func init() {
+	loadTodoList()
+}
+
+func loadTodoList() {
+	file, err := ioutil.ReadFile("file.csv")
+	if err != nil {
+		panic(err)
+	}
+	_ = json.Unmarshal(file, &Lst)
+}
